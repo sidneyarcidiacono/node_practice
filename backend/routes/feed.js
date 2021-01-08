8e9c175cf07861196573e77b9987f8f200be69ee
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-08 10:44:33 -0800

    Added authorization to all feed routes, front end handlers.

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const { body } = require('express-validator/check');
+const express = require('express')
+const { body } = require('express-validator/check')
 
-const feedController = require('../controllers/feed');
+const feedController = require('../controllers/feed')
+const isAuth = require('../middleware/is-auth')
 
-const router = express.Router();
+const router = express.Router()
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts)
 
 // POST /feed/post
 router.post(
   '/post',
+  isAuth,
   [
     body('title')
       .trim()
@@ -20,12 +22,13 @@ router.post(
       .isLength({ min: 5 })
   ],
   feedController.createPost
-);
+)
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost)
 
 router.put(
   '/post/:postId',
+  isAuth,
   [
     body('title')
       .trim()
@@ -35,8 +38,8 @@ router.put(
       .isLength({ min: 5 })
   ],
   feedController.updatePost
-);
+)
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost)
 
-module.exports = router;
+module.exports = router
