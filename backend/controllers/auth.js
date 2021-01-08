56ac2545477ff170ad974b89a8d3cdd3fa9ddb01
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-08 09:19:37 -0800

    Updated signup route to add user to db

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 const { validationResult } = require('express-validator/check')
 
@@ -14,6 +15,24 @@ exports.signup = (req, res, next) => {
   const email = req.body.email
   const name = req.body.name
   const password = req.body.password
+  bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        name: name
+      })
+      return user.save()
+    })
+    .then(result => {
+      res.status(201).json({message: 'User created!', userId: result._id})
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
 
-  // TODO: implement password hashing, add user to database
+
 }
